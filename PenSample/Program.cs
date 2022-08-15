a0c81518459477d7cf2f436b2b31d63a411ae5d9
Jacob Garcia <jacobgarcia@rocketmortgage.com>
2022-08-15 13:23:58 -0400

    after lunch push

---
+++ PenSample/Program.cs
@@ -0,0 +1,105 @@
+﻿// firstPen is an object that has inside it 4 variables. 
+Pen firstPen = new Pen();
+firstPen.inkColor = "blue";
+firstPen.length = 4.5;
+firstPen.pointType = "gel";
+firstPen.pointSize = "small";
+firstPen.inkAmount = 5;
+// calling the pen variables that we set 
+Console.WriteLine("First Pen");
+Console.WriteLine(firstPen.inkColor);
+Console.WriteLine(firstPen.length);
+Console.WriteLine(firstPen.pointType);
+Console.WriteLine(firstPen.pointSize);
+Console.WriteLine(firstPen.inkAmount);
+// using methods that we made just for pen
+Console.WriteLine("Lets draw with first pen.");
+firstPen.Draw();
+Console.WriteLine($"The ink is now {firstPen.inkAmount}.");
+
+Console.WriteLine();
+//Second Pen
+Pen secondPen = new Pen();
+secondPen.inkColor = "red";
+secondPen.length = 4.5;
+secondPen.pointType = "ball";
+secondPen.pointSize = "medium";
+secondPen.inkAmount = 2;
+Console.WriteLine("Second Pen");
+Console.WriteLine(secondPen.inkColor);
+Console.WriteLine(secondPen.length);
+Console.WriteLine(secondPen.pointType);
+Console.WriteLine(secondPen.pointSize);
+Console.WriteLine(secondPen.inkAmount);
+Console.WriteLine("Lets draw with second Pen twice");
+secondPen.Draw();
+secondPen.Draw();
+
+Console.WriteLine($"Second pen now has {secondPen.inkAmount} and First Pen has {firstPen.inkAmount}");
+
+secondPen.Draw();
+Console.WriteLine($"Second pen now has {secondPen.inkAmount} and First Pen has {firstPen.inkAmount}");
+secondPen.Refill();
+Console.WriteLine($"Second pen now has {secondPen.inkAmount} and First Pen has {firstPen.inkAmount}");
+
+Console.WriteLine();
+Console.WriteLine("Refilling first pen with green ink...");
+firstPen.Refill("Pink");
+Console.WriteLine($"First pen has {firstPen.inkColor} ink and has {firstPen.inkAmount} ink points");
+// third pen created and ink amount set to 10 by the constructer below 
+Console.WriteLine();
+Pen thirdPen = new Pen();
+Console.WriteLine(thirdPen.inkAmount);
+
+Console.WriteLine();
+Console.WriteLine("Fourth Pen");
+Pen fourthPen = new Pen("Orange");
+Console.WriteLine(fourthPen.inkAmount);
+Console.WriteLine(fourthPen.inkColor);
+
+class Pen
+{
+    public string inkColor;
+    public double length;
+    public string pointType;
+    public string pointSize;
+    public int inkAmount;
+
+
+    public Pen() // default constructor
+    {
+        inkAmount = 10;
+        inkColor = "black";
+    }
+
+    public Pen(string _inkColor) //things and stuff here 
+    {
+        inkColor = _inkColor;
+        inkAmount = 10;
+    }
+
+    // member methods
+    public void Draw()
+    {
+        if (inkAmount > 0)
+        {
+            inkAmount--;
+        }
+
+    }
+
+    public void Refill()
+    {
+        inkAmount = 10;
+    }
+
+    // adding another refill function, that lets us change ink color.
+    public void Refill(string newColor)
+    {
+        inkColor = newColor;
+        inkAmount = 10;
+    }
+
+}
+
+
---
+++ PenSample2/Program.cs
@@ -0,0 +1,70 @@
+﻿// Pen gelPen = new Pen("red",5.0,"gel","medium");
+Pen gelPen = new Pen("green",5.0,"gel","medium");
+
+gelPen.Print();
+Console.WriteLine();
+Pen feltPen = new Pen("blue", 4, "felt", "small");
+
+feltPen.Print();
+
+gelPen.Draw(5);
+gelPen.Print();
+gelPen.Draw(7);
+gelPen.Print();
+gelPen.Refill();
+gelPen.Print();
+Console.WriteLine();
+
+Console.WriteLine(gelPen);
+
+class Pen
+{
+    public string inkColor;
+    public double length;
+    public string pointType;
+    public string pointSize;
+    public int inkAmount;
+
+    public Pen(string _inkColor, double _length, string _pointType, string _pointSize )
+    {
+        inkColor = _inkColor;
+        length = _length;
+        pointType = _pointType;
+        pointSize = _pointSize;
+        inkAmount = 10;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("This pen:");
+        Console.WriteLine($"   Color:{inkColor} Length:{length} Type:{pointType} Size{pointSize} amount of Ink: {inkAmount}");
+    }
+
+    public void Draw(int lineLength)
+    {
+        if (lineLength > inkAmount)
+        {
+            Console.WriteLine("Sorry, not enough ink for that.");
+        }
+        else
+        {
+            inkAmount -= lineLength;
+            if (inkAmount < 0)
+            {
+                inkAmount = 0;
+            }
+        }
+
+    }
+
+    public void Refill()
+    {
+        inkAmount = 10;
+    }
+
+    public override string ToString()
+    {
+        return $"Color:{inkColor}   Length:{length}     Type:{pointType}    Size:{pointSize}    Amount:{inkAmount}";
+    }
+
+}
